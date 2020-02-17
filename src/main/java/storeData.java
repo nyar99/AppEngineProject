76935b4e1c7e55887a21efab22ae3554a53b89df
unknown <nyar99@gmail.com>
2020-02-17 01:31:09 -0600

    added picture functionality

--- src/main/java/storeData.java
+++ src/main/java/storeData.java
@@ -2,11 +2,9 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
@@ -15,6 +13,9 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 
 @WebServlet(
 	    name = "storeData",
@@ -22,23 +23,25 @@ import com.google.appengine.api.datastore.KeyFactory;
 	)
 
 public class storeData extends HttpServlet{
+	final String Bucket = "cookingwithchris.appspot.com";
     private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-
+    private ImagesService imageService = ImagesServiceFactory.getImagesService();
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException,IOException{
 		String title=req.getParameter("title");
 		String content=req.getParameter("content");
-		Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-        List<BlobKey> blobKeys = blobs.get("food");
 		Date date = new Date();
 		Key postKey = KeyFactory.createKey("Blogpost","default");
 		Entity post = new Entity("post",postKey);
 		post.setProperty("title", title);
 		post.setProperty("content", content);
 		post.setProperty("date", date);
-		if(!blobKeys.isEmpty()) {
-        	String blobToServe = blobKeys.get(0).getKeyString();
-        	post.setProperty("blob-key", blobToServe);
+		Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
+        List<BlobKey> blobKeys = blobs.get("food");
+        if(!blobKeys.isEmpty()) {
+        	ServingUrlOptions servingUrlOptions = ServingUrlOptions.Builder.withBlobKey(blobKeys.get(0));
+        	String image_url = imageService.getServingUrl(servingUrlOptions);
+        	post.setProperty("image", image_url);
         }
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		datastore.put(post);
--- src/main/webapp/landing.jsp
+++ src/main/webapp/landing.jsp
@@ -1,29 +1,19 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-
 <%@ page import="java.util.List" %>
-
 <%@ page import="com.google.appengine.api.users.User" %>
-
 <%@ page import="com.google.appengine.api.users.UserService" %>
-
 <%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
-
 <%@ page import="com.google.appengine.api.datastore.DatastoreServiceFactory" %>
-
 <%@ page import="com.google.appengine.api.datastore.DatastoreService" %>
-
 <%@ page import="com.google.appengine.api.datastore.Query" %>
-
 <%@ page import="com.google.appengine.api.datastore.Entity" %>
-
 <%@ page import="com.google.appengine.api.datastore.FetchOptions" %>
-
 <%@ page import="com.google.appengine.api.datastore.Key" %>
-
 <%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
-
+<%@ page import="com.google.appengine.api.blobstore.BlobKey" %>
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreService" %>
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreServiceFactory" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-
  <%
  	UserService userService = UserServiceFactory.getUserService();
 	User user = userService.getCurrentUser(); 
@@ -56,17 +46,22 @@
 %>
 	
 
-<% 
+<% 	
 	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Key postsKey = KeyFactory.createKey("Blogpost", "default");
     Query query = new Query("post", postsKey).addSort("date", Query.SortDirection.DESCENDING);
     List<Entity> recipes = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(3));
    	for(Entity r : recipes){
+   		pageContext.setAttribute("r",r);
    		pageContext.setAttribute("recipe", r.getProperty("title"));
    		pageContext.setAttribute("instructions", r.getProperty("content"));
    		%>
    		<blockquote>${recipe}</blockquote>
    		<p>${instructions}<p>
+   		<% if(r.getProperty("image")!=null){
+   			pageContext.setAttribute("image",r.getProperty("image"));
+   			%> <img src=${image}}> <%
+   		} %>
    		<%
    	}
  %>
