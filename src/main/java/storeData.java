8de481aa7853617961f338dd004cf86462c60824
unknown <nyar99@gmail.com>
2020-02-16 21:39:26 -0600

    Naveen driving, starting to add image upload

--- src/main/java/storeData.java
+++ src/main/java/storeData.java
@@ -1,8 +1,15 @@
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -15,16 +22,24 @@ import com.google.appengine.api.datastore.KeyFactory;
 	)
 
 public class storeData extends HttpServlet{
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-	throws IOException{
+	throws ServletException,IOException{
 		String title=req.getParameter("title");
 		String content=req.getParameter("content");
+		Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
+        List<BlobKey> blobKeys = blobs.get("food");
 		Date date = new Date();
 		Key postKey = KeyFactory.createKey("Blogpost","default");
 		Entity post = new Entity("post",postKey);
 		post.setProperty("title", title);
 		post.setProperty("content", content);
 		post.setProperty("date", date);
+		if(!blobKeys.isEmpty()) {
+        	String blobToServe = blobKeys.get(0).getKeyString();
+        	post.setProperty("blob-key", blobToServe);
+        }
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		datastore.put(post);
 		resp.sendRedirect("/landing.jsp");
--- src/main/webapp/createPost.jsp
+++ src/main/webapp/createPost.jsp
@@ -1,12 +1,16 @@
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreService" %>
 <html>
+	<%BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();%>
 	<head>
 	
 	</head>
 	<body>
-		<form action="/store" method="post">
+		<form action="<%= blobstoreService.createUploadUrl("/store")%>" method="post" enctype="multipart/form-data">
 			<div><textarea name="title" rows="1" cols="60"></textarea></div>
 			<div><textarea name="content" rows="3" cols="60"></textarea></div>
-			<div><input type="submit" value="Post Greeting" ></div>
+			<div><input type="file" name="food"></div>
+			<div><input type="submit" value="Post Recipe" ></div>
 		</form>
 		<a href="/landing.jsp">Cancel Post</a>
 	</body>
